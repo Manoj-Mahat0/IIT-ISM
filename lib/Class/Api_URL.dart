b6afc47ef7c0b003227d34fa344a049c5d09521c
Manoj-Mahat0 <codeinlastbench@gmail.com>
2025-01-26 14:12:38 +0530

    first commit

---
+++ lib/Class/Api_URL.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: file_names
+class ApiConstants {
+  static const String baseUrl = 'https://apps.iitism.ac.in/newtest/api/';
+  static const String baseUrllogin = 'apps.iitism.ac.in';
+  static const String endpointotp = '/newtest/Admin_sendOTP';
+  static const String endpointotpnew = '/newtest/api/newPassword_verify';
+  // static const String endpointotpnew = '/api/Password_verify';
+  static const String endpointverify = '/newtest/api/Admin_verifyOTP';
+}
+
+// class ApiConstants {
+//   static const String baseUrl = 'https://apps.iitism.ac.in/api/';
+//   static const String baseUrllogin = 'apps.iitism.ac.in';
+//   static const String endpointotp = '/api/Admin_sendOTP';
+//   static const String endpointotpnew = '/api/newPassword_verify';
+//   // static const String endpointotpnew = '/api/Password_verify';
+//   static const String endpointverify = '/api/Admin_verifyOTP';
+// }
+
+// class ApiConstants {
+//   static const String baseUrl =
+//       'https://maaalankarjewellers.com/ismapinew/api/';
+//   static const String baseUrllogin = 'www.maaalankarjewellers.com';
+//   static const String endpointotp = '/ismapinew/api/Admin_sendOTP';
+//   static const String endpointverify = '/ismapinew/api/Admin_verifyOTP';
+// }
+// class ApiConstants {
+//   static const String baseUrl = 'https://bazar11.com/ismapinew/api/';
+//   static const String baseUrllogin = 'www.bazar11.com';
+//    static const String endpointotpnew = '/ismapinew/api/Password_verify';
+//   static const String endpointotp = '/ismapinew/api/Admin_sendOTP';
+//   static const String endpointverify = '/ismapinew/api/Admin_verifyOTP';
+// }
